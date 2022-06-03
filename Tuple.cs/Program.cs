41e581382b863c988b8add9831aea1982e3910cd
Mohantysuman <105624690+Mohantysuman@users.noreply.github.com>
2022-06-03 16:57:16 +0530

    Add files via upload

---
+++ Tuple.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿// C# program to create tuple using tuple constructor.
+using System;
+
+public class Tuplehandson
+{
+
+	// Main method
+	static public void Main()
+	{
+
+		// Tuple with one element
+		Tuple<string> My_Tuple1 = new Tuple<string>("Kelltontech");
+
+		// Tuple with three elements
+		Tuple<string, string, int> My_Tuple2 = new Tuple<string, string, int>("Romil", "xyz", 29);
+
+		// Tuple with eight elements
+		Tuple<int, int, int, int, int, int, int, Tuple<int>> My_Tuple3 = new Tuple<int, int, int, int, int, int, int, Tuple<int>>(1, 2, 3, 4, 5, 6, 7, new Tuple<int>(8));
+	}
+}
